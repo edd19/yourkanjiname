57ea4e1f050dd95903f0df9cd0242cc9c21ebbdc
Ndizera Eddy <eddyndizera@gmail.com>
2021-05-15 09:21:55 +0200

    Refactor

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function displayKanjiName(event) {
     event.preventDefault();
-
-    kanjiParagraph.innerHTML = "";
+    KanjiTable.innerHTML = "";
 
     const kanjis = [...inputName.value].map((katakana) => getKanji(katakana));
     const rows = kanjis.map((kanji) => createKanjiRow(kanji))
-
-    rows.forEach(row => kanjiParagraph.appendChild(row))
+    rows.forEach(row => KanjiTable.appendChild(row))
 }
 
 function createKanjiRow(kanji) {
@@ -25,17 +23,9 @@ function createKanjiRow(kanji) {
     return newRow;
 }
 
-function convertKatakanaNameToKanji(katakanaName) {
-    return [...katakanaName].map((katakana) => getRandomElement(data[katakana]).literal).join('')
-}
-
 function getKanji(katakana) {
     const randomKanji = getRandomElement(data[katakana])
-    return {
-        'katakana': katakana,
-        'kanji': randomKanji.literal,
-        'meaning': randomKanji.meaning
-    }
+    return {'katakana': katakana, 'kanji': randomKanji.literal, 'meaning': randomKanji.meaning}
 }
 
 function getRandomElement(arrayObject) {
@@ -59,8 +49,9 @@ function saveKanjiName() {
 
 const form = document.getElementById("form");
 const inputName = document.getElementById("inputName");
-const kanjiParagraph = document.getElementById('kanjiArea');
-const kanjiArea = document.getElementsByClassName('kanjiParagraph')[0]
+const KanjiTable = document.getElementById('tableKanji');
+const kanjiArea = document.getElementsByClassName('divKanji')[0]
 const btnSave = document.getElementById('btnSave');
+
 form.addEventListener('submit', displayKanjiName);
 btnSave.onclick = saveKanjiName
